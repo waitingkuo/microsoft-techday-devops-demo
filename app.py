7b21036ceed52189341d8b56233de584c2cd1211
Wei-Ting Kuo <waitingkuo0527@gmail.com>
2015-09-17 03:44:25 +0800

    add flask

--- app.py
+++ app.py
@@ -1,3 +1,31 @@
-def add(x, y):
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    x = request.args.get('x')
+    y = request.args.get('y')
+
+    try:
+        result = add(int(x) , int(y))
+    except ValueError:
+        result = ''
+    
+    return '''
+<body>
+    <form action="/" method="get">
+
+        <input name="x" type="text"> + <input name="y" type="text"> = %s
+        <br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+''' % result
+
+
+def add(x, y):
     return x+y
+
+if __name__ == '__main__':
+    app.run(debug=True)
